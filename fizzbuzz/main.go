246deb1a1889aa129094d2ece2da0b53dd63de74
Derek Fodeke-Rodgers <Dmonarch134@gmail.com>
2025-02-08 22:40:32 -0600

    new concepts

---
+++ fizzbuzz/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// the classic fizzbuzz game
+// prints the numbers 1-100  inclusive each on their own line, but substitues multipes of 3 for the text fizz and nultiples of 5 for buzz
+// for multiples of 3 and 5 print fizzbuzz
+func fizzbuzz() {
+	for i := 0; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
+func main() {
+	fizzbuzz()
+}

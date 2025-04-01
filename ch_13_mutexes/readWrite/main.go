bbaa27574a2c4cf01a45728ee659935fb16403ab
Derek Fodeke-Rodgers <Dmonarch134@gmail.com>
2025-03-31 22:29:34 -0500

    rlocks

---
+++ ch_13_mutexes/readWrite/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+/*
+Lets update our code from the last assignment  but this time we can speed it up by
+allowing readers to read from the map concurrently
+run the test suite  you'll notice it takes forever
+Update the val method to only lock the mutex for reading
+*/
+
+type safeCounter struct {
+	counts map[string]int
+	mu     *sync.RWMutex
+}
+
+func (sc safeCounter) inc(key string) {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	sc.slowIncrement(key)
+}
+
+func (sc safeCounter) val(key string) int {
+	sc.mu.RLock()
+	defer sc.mu.RUnlock()
+	return sc.counts[key]
+}
+
+// don't touch below this line
+
+func (sc safeCounter) slowIncrement(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
